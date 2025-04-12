becc60e6cc359b5031bcedf3a0bbeb7fed06615e
Alana <alanac248@gmail.com>
2025-04-12 17:39:32 -0300

    Criando repositório

---
+++ src/app/components/exercicios/exercicios.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exercicios',
+  imports: [],
+  templateUrl: './exercicios.component.html',
+  styleUrl: './exercicios.component.css'
+})
+export class ExerciciosComponent {
+
+}
