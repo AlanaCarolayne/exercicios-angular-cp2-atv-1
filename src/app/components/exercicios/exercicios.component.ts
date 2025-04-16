be5d56743f312ed80fc701852d580ba2d8a14dcc
Alana <alanac248@gmail.com>
2025-04-16 00:50:37 -0300

    Atualizado até exercicio 5

--- src/app/components/exercicios/exercicios.component.ts
+++ src/app/components/exercicios/exercicios.component.ts
@@ -1,11 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-exercicios',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './exercicios.component.html',
-  styleUrl: './exercicios.component.css'
+  styleUrl: './exercicios.component.css',
 })
 export class ExerciciosComponent {
+  mes: number = 1;
+  itens = ['Maçã', 'Banana', 'Leite'];
+  idade: number = 19;
+  tarefas = ['Estudar Angular', 'Fazer exercicios', 'Revisar Código'];
+  status: string = '';
+  trocarValor(event: Event): void {
+    const elemento = event.target as HTMLInputElement;
+    this.mes = parseInt(elemento.value);
+  }
 
+  aumenta(): void {
+    this.idade++;
+  }
+  diminui(): void {
+    this.idade--;
+  }
+  eliminaTarefa(nome: string): void {
+    const index = this.tarefas.indexOf(nome);
+    this.tarefas.splice(1, index);
+  }
+  trocarStatus(event:Event) : void{
+    const elemento = event.target as HTMLInputElement
+    this.status = elemento.value
+      }
 }
